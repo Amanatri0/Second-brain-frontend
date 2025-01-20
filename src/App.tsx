89686cbfa49d6d1ad54436f8a5718183112669cd
Amanatri0 <atriaman8897@gmail.com>
2025-01-20 21:37:46 +0530

    Sidebar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import { Sidebar } from "./components/SideBar";
 import { VideoGridItems } from "./components/VideGridItems";
+import { SidebarProvider } from "./content/sidebarcontent";
 import { videos } from "./data/data";
 import { PageHeader } from "./layouts/PageHeader";
 
 export default function App() {
   return (
-    <div className="max-h-screen flex flex-col">
-      <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <Sidebar />
-        <div className="overflow-x-hidden px-8 pb-4">
-          {/* <div className="sticky top-0 bg-white z-10 pb-4"> */}
-          {/* </div> */}
-          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
-            {videos.map((video) => (
-              <VideoGridItems tittle={"something"} key={video.id} {...video} />
-            ))}
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            {/* <div className="sticky top-0 bg-white z-10 pb-4"> */}
+            {/* </div> */}
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video) => (
+                <VideoGridItems
+                  tittle={"something"}
+                  key={video.id}
+                  {...video}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
--- src/content/sidebarcontent.tsx
+++ src/content/sidebarcontent.tsx
@@ -1,13 +1,59 @@
-// import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
-// type SidebarProviderProps = {
-//   children: ReactNode;
-// };
+type SidebarProviderProps = {
+  children: ReactNode;
+};
 
-// const SidebarContext = createContext(null);
+type SidebarContextType = {
+  isLargeOpen: boolean;
+  isSmallOpen: boolean;
+  toggle: () => void;
+  close: () => void;
+};
 
-// export function SidebarProvider({ children }: SidebarProviderProps) {
-//   return (
-//     // <SidebarContext.Provider value={{}}>{children}</SidebarContext.Provider>
-//   );
-// }
+const SidebarContext = createContext<SidebarContextType | null>(null);
+
+export function useSideBarContext() {
+  const value = useContext(SidebarContext);
+
+  if (value == null) throw Error("Cannot use outside of SidebarProvider");
+
+  return value;
+}
+
+export function SidebarProvider({ children }: SidebarProviderProps) {
+  const [isLargeOpen, setIsLargeOpen] = useState(true);
+  const [isSmallOpen, setIsSmallOpen] = useState(false);
+
+  function isScreenSmall() {
+    return window.innerWidth < 1024;
+  }
+
+  function toggle() {
+    if (isScreenSmall()) {
+      setIsSmallOpen((s) => !s);
+    } else {
+      setIsLargeOpen((l) => !l);
+    }
+  }
+  function close() {
+    if (isScreenSmall()) {
+      setIsSmallOpen(false);
+    } else {
+      setIsLargeOpen(false);
+    }
+  }
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        isLargeOpen,
+        isSmallOpen,
+        toggle,
+        close,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
+}
